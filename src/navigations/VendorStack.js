adc9de57f25338ff8109284cfdd6b73af457dffd
2022gma <sidb7248@gmail.com>
2022-03-28 21:47:01 +0530

    changes on navigation

--- src/navigations/VendorStack.js
+++ src/navigations/VendorStack.js
@@ -1,34 +1,33 @@
 import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { VendorHome, ManageProducts, SelectFromStore } from './../screens';
-
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {VendorHome, ManageProducts, SelectFromStore} from './../screens';
 
 const VendorStk = createNativeStackNavigator();
 
-export default function VendorStack({ route, navigation }) {
-    return (
-        <VendorStk.Navigator initialRouteName='VendorHome'>
-            <VendorStk.Screen
-                name="VendorHome"
-                component={VendorHome}
-                options={{
-                    headerShown: false,
-                }}
-            />
-            <VendorStk.Screen
-                name="ManageProducts"
-                component={ManageProducts}
-                options={{
-                    headerShown: false,
-                }}
-            />
-            <VendorStk.Screen
-                name="SelectFromStore"
-                component={SelectFromStore}
-                options={{
-                    headerShown: false,
-                }}
-            />
-        </VendorStk.Navigator>
-    );
+export default function VendorStack({route, navigation}) {
+  return (
+    <VendorStk.Navigator initialRouteName="ManageProducts">
+      {/* <VendorStk.Screen
+        name="VendorHome"
+        component={VendorHome}
+        options={{
+          headerShown: false,
+        }}
+      /> */}
+      <VendorStk.Screen
+        name="ManageProducts"
+        component={ManageProducts}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <VendorStk.Screen
+        name="SelectFromStore"
+        component={SelectFromStore}
+        options={{
+          headerShown: false,
+        }}
+      />
+    </VendorStk.Navigator>
+  );
 }
