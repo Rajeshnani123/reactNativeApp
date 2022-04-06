0e2aa5319287a5976d934d1c4e2c1dc3fb8663e3
pramodSheelvant <pramodsheelvant@gmail.com>
2022-04-06 11:53:37 +0530

    doing logn

---
+++ src/constants/prodetails.js
@@ -0,0 +1,8 @@
+const Data= {
+    name:'Proname',
+    qty:'Quantity',
+    item:'ItemType',
+    retail:'Rer'
+}
+
+export default Data;
--- src/screens/enduser/ProductDetails.js
+++ src/screens/enduser/ProductDetails.js
@@ -1,7 +1,10 @@
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity, Image, ScrollView} from 'react-native';
 import React, {useState} from 'react';
 import style from '../style';
 import Icon from 'react-native-vector-icons';
+import plateform from '../../../src/utils/Platform';
+import prodetails from '../../../src/constants/prodetails';
+import {Review} from '../enduser/Review';
 import {
   Header,
   BackHeader,
@@ -13,9 +16,9 @@ import {
   HorizontalCard,
   ActionBtn,
   CustomModal,
-  constants
+  constants,
 } from '../../components';
-import { COLORS, FONTS, SIZES } from '../../constants';
+import {COLORS, FONTS, SIZES} from '../../constants';
 import styles from '../../components/basics/styles';
 
 const HeaderContent = ({navigation}) => {
@@ -74,26 +77,124 @@ const rightComponent = () => {
     </View>
   );
 };
-const names = {
-  lname:"Reviews",
-  rname:"Compare"
-}
-let {lname,rname}=names
+
+const ReviewsComponent = () => {
+  return (
+    <View style={{width: 180}}>
+      <TouchableOpacity
+        style={{
+          borderColor: '#000',
+          borderBottomWidth: 2,
+          borderBottomColor: '#ffb43a',
+          height: 45,
+        }}>
+        <Text
+          style={{
+            marginHorizontal: 50,
+            fontFamily: 'Poppins',
+            fontWeight: '600',
+            fontSize: 18,
+            marginVertical: 8,
+          }}>
+          Reviews
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const CompareComponent = () => {
+  return (
+    <View style={{width: 180}}>
+      <TouchableOpacity>
+        <Text
+          style={{
+            marginHorizontal: 40,
+            fontFamily: 'Poppins',
+            fontWeight: '600',
+            fontSize: 18,
+            marginVertical: 8,
+          }}>
+          Compare
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 const ProductDetails = ({navigation}) => {
   const [search, setSearch] = useState();
-  const [isModelOpen, setIsModelOpen] = useState(false);
- 
+
+  const Data = {
+    name: 'Product name',
+    qty: 'Quantity',
+    item: 'ItemType',
+    retail: 'Retailer',
+    price: 'Price',
+  };
+
   return (
     <View>
-      <HeaderContent navigation={navigation} />
-      <View style={{flexDirection:'row',padding:20}}>
-       <Text style={{fontSize:16,marginHorizontal:35}}>{lname}</Text>
-       <Text style={{fontSize:16,marginHorizontal:50}}>{rname}</Text>
+      <ScrollView>
+        <HeaderContent navigation={navigation} />
+
+        <View style={{flexDirection: 'row'}}>
+          <ReviewsComponent />
+          <CompareComponent />
+        </View>
+
+        <Text
+          style={{
+            marginHorizontal: 50,
+            fontFamily: 'Poppins',
+            fontWeight: '600',
+            fontSize: 18,
+            marginVertical: 20,
+            color: '#000',
+          }}>
+          Product Details
+        </Text>
+        <View>
+          <Image
+            source={require('../../Assets/Images/prodec.png')}
+            style={{
+              height: 230,
+              width: 300,
+              marginHorizontal: 30,
+              marginVertical: 20,
+            }}
+          />
+        </View>
+        <View style={{marginHorizontal: 50}}>
+          <Text style={{marginVertical: 10}}>{Data.name}</Text>
+          <Text style={{marginVertical: 10}}>{Data.qty}</Text>
+          <Text style={{marginVertical: 10}}>{Data.retail}</Text>
+          <Text style={{marginVertical: 10}}>{Data.item}</Text>
+          <Text style={{marginVertical: 10}}>{Data.price}</Text>
+        </View>
+        
+        <View style={{marginLeft: 20, marginVertical: 20}}>
+        <Text style={FONTS.h2}>Reviews</Text>
+          {Review.map(item => (
+            <View
+              style={{
+                marginTop:20,
+                flexDirection: 'row',
+                marginVertical: 10,
+                marginRight:20
+              }}>
+              <Image source={item.image} />
+              <View style={{marginRight: 60}}>
+                <Text style={{marginLeft: 20,fontWeight:'bold'}} >{item.name}</Text>
+                <Text style={{marginLeft: 20,fontSize:14,marginTop:5}}>{item.desc}</Text>
+                <Text style={{marginLeft: 20,marginTop:5}}>{item.rating}</Text>
+                <Text style={{marginLeft:180,fontSize:12}}>{item.time}</Text>
+              </View>
+            </View>
+          ))}
+        </View>
+      </ScrollView>
     </View>
-    <Text >Product Details</Text>
-    </View>
-    
   );
 };
 
