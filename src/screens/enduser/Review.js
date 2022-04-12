57be88b7e66d7340b541696f9c01f17a5841552f
pramodSheelvant <pramodsheelvant@gmail.com>
2022-04-12 16:20:43 +0530

    assets

--- src/screens/enduser/Review.js
+++ src/screens/enduser/Review.js
@@ -2,20 +2,20 @@
 
 export const Review=[
     {
-        image:require('../../Assets/Images/r1.png'),
+        image:require('../../assets/Images/r1.png'),
         name:'Advit',
         rating:'4 / 5',
         time:'5min ago',
         desc:'Lorem ipsum dolor sit adipiscing elit, sed do eiusmod tempor incididunt ut labore '
     },
     {
-        image:require('../../Assets/Images/r2.png'),
+        image:require('../../assets/Images/r2.png'),
         rating:'3 / 5',
         time:'20min ago',
         desc:'Lorem ipsum dolor sit adipiscing elit, sed do eiusmod tempor incididunt ut labore'
     },
     {
-        image:require('../../Assets/Images/r3.png'),
+        image:require('../../assets/Images/r3.png'),
         name:'Arun',
         rating:'5 / 5',
         time:'50min ago',
